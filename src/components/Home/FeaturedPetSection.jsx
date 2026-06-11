bee738acbc229ebf4fd65921a553f91e70c422e6
karim-ullah <freelancerkarimullah@gmail.com>
2026-06-11 15:00:32 +0600

    modified cache issue

--- src/components/Home/FeaturedPetSection.jsx
+++ src/components/Home/FeaturedPetSection.jsx
@@ -4,7 +4,9 @@ import PetCard from "../shared/PetCard";
 
 const FeaturedPetSection = async() => {
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/pets`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/pets`,{
+      cache: 'no-store'
+    })
     const pets = await res.json()
 
   return (
